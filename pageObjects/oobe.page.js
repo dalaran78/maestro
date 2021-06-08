ed0f978fa7e0a7c8347b78e06bdfe63790a7afe9
Sergei Svistelnikov <sergei.svistelnikov@outlook.com>
2021-06-07 18:24:58 -0700

    OOBE FastPairing cases done

--- pageObjects/oobe.page.js
+++ pageObjects/oobe.page.js
@@ -1,10 +1,8 @@
 class halfsheet {
-    /**
-     * Define Pairing Elements
-     */
+    // #Halfsheet Pairing elements
     get connectBtn() {return $('//android.widget.Button[@resource-id="com.google.android.gms:id/connect_btn"]')};
     get pairBtn() {return $('//android.widget.Button[@resource-id="android:id/button1"]')};
-    get setupDevice() {return $('//android.widget.TextView[@content-desc="Set up device"]')};
+    get setupDevice() {return $('//android.widget.TextView[@resource-id="com.google.android.gms:id/toolbar_title"]')};
 
     _setupDeviceHalfsheet(index){
         return $(`//android.widget.TextView[@index="${index}"]`);
@@ -15,9 +13,11 @@ class halfsheet {
     get setupBtn() {return $('//android.widget.Button[@resource-id="com.google.android.gms:id/setup_btn"]')};
     get linkPixelMsg() {return $('//android.widget.LinearLayout[@resource-id="com.android.companiondevicemanager:id/container"]')};
     get okBtn() {return $('//android.widget.Button[@resource-id="com.android.companiondevicemanager:id/button_pair"]')};
-    // Start OOBE, I accept, Next,  buttonds has same recource-id
-    get startSetupBtn() {return $('//android.widget.Button[@resource-id="com.google.android.apps.wearables.maestro.companion:id/oobe_action_button"]')};
+    // Start OOBE, I accept, Next  buttonds has same recource-id
+    get oobeActionBtn() {return $('//android.widget.Button[@resource-id="com.google.android.apps.wearables.maestro.companion:id/oobe_action_button"]')};
     get cancelBtn() {return $('//android.widget.Button[@resource-id="com.google.android.apps.wearables.maestro.companion:id/oobe_cancel_button"]')};
 
+    // #Assistant Flow Activity
+    get continueBtn() {return $('//android.widget.Button[@resource-id="com.google.android.apps.wearables.maestro.companion:id/oobe_continue_button"]')};
     }
     module.exports = new halfsheet();
--- test/specs/oobeFastPair_test.js
+++ test/specs/oobeFastPair_test.js
@@ -1,44 +1,75 @@
 const objects = require('../../pageObjects/oobe.page');
-// const data = require('../../data/data');
 const expected = require('../../data/expected');
+// const methods = require('../../helpers/methods');
 const expect = require('chai').expect;
 
-describe('OOBE FastPair test suite', ()=>{
+describe('OOBE FLOW', ()=>{
     // Execute a block of code before every test
     beforeEach(() => {
     });
 
-    it('TC-001 | Halfsheet pairing notification shoud be shown on screen', async ()=>{
-        await (await (objects.connectBtn)).waitForDisplayed({ timeout: 5000 });
+    describe('OOBE FastPair test suite', () => {
+
+        it('TC-001 | Halfsheet pairing notification shoud be shown on screen', async ()=>{
+            await (await (objects.connectBtn)).waitForDisplayed({ timeout: 5000 });
+        });
+
+        it('TC-002 | Stretto should be paired after "Connect" Btn click', async ()=>{
+            await (await (objects.connectBtn)).click()
+            await (await (objects.setupDevice)).isDisplayed();
+            text = await (await objects._setupDeviceHalfsheet(0)).getText();
+            expect(text).equal(expected.setupDeviceHalfsheet.title);
+        });
+
+        it('TC-003 | "Set Up" notification should be shown on screen and clickable', async ()=>{
+            await (await (objects.setupBtn)).waitForDisplayed({ timeout: 5000 });
+            await (await (objects.setupBtn)).click();
+        });
+
+        it('TC-004 | Link Pixel Buds with "user\'s acc name" message should be shown on screen', async ()=>{
+            await (await (objects.linkPixelMsg)).waitForDisplayed({ timeout: 12000 });
+            text = await (await objects._setupDeviceHalfsheet(0)).getText();
+            expect(text).equal(expected.linkPixelBuds.msg);
+        });
+
+        it('TC-005 | Link Pixel Buds with... "OK" button should send user to OOBE Welcome screen', async ()=>{
+            await (await (objects.okBtn)).click();
+        });
+
+        // Case skipped FAIL, CANCEL button not clickable
+        it.skip('TC-006 | Link Pixel Buds with... "CANCEL" button should cancel OOBE flow', async ()=>{
+            await (await (objects.cancelBtn)).click();
+        });
+
+        it('TC-007 | "Start setup" > "I accept" > "Next" > "Next" buttons click', async ()=>{
+        let multipleClick;
+            for (i = 0; i < 4; i++){
+                //4 clicks the buttons
+                await (await (objects.oobeActionBtn)).click();
+            };  return multipleClick;
+
+        });
+
+        it('TC-008 | Continue OOBE flow .........', async ()=>{
+            let multipleClick;
+            await (await (objects.oobeActionBtn)).click();
+            await (await (objects.oobeActionBtn)).waitForDisplayed({ timeout: 6000 });
+            for (i = 0; i < 6; i++){
+                //7 clicks the buttons
+                await (await (objects.oobeActionBtn)).click();
+            };  return multipleClick;
+
+        });
+
     });
 
-    it('TC-002 | Stretto should be paired after "Connect" button click', async ()=>{
-        await (await (objects.connectBtn)).click()
-        await (await (objects.setupDevice)).isDisplayed();
-        text = await (await objects._setupDeviceHalfsheet(0)).getText();
-        expect(text).equal(expected.setupDeviceHalfsheet.title);
+    describe('Assistant flow test suite', () => {
+
+        it('TC-001 | Halfsheet pairing notification shoud be shown on screen', async ()=>{
+            await (await (objects.connectBtn)).waitForDisplayed({ timeout: 5000 });
+        });
+
     });
-
-    it('TC-003 | Link Pixel Buds with "user\'s acc name" message should be shown on screen', async ()=>{
-        await (await (objects.linkPixelMsg)).isDisplayed();
-        text = await (await objects._setupDeviceHalfsheet(0)).getText();
-        expect(text).equal(expected.linkPixelBuds.msg);
-    });
-
-    it('TC-004 | Link Pixel Buds with... "OK" button should send user to OOBE Welcome screen', async ()=>{
-        await (await (objects.okBtn)).click()
-    });
-
-    // Case skipped FAIL, CANCEL button not clickable
-    it.skip('TC-005 | Link Pixel Buds with... "CANCEL" button should cancel OOBE flow', async ()=>{
-        await (await (objects.cancelBt)).click()
-    });
-
-    // it.skip('TC-004 | "Set Up" propose notification should be shown on screen and clickable', async ()=>{
-    //     await (await (objects.setupBtn)).waitForDisplayed({ timeout: 9000 });
-    //     await (await (objects.setupBtn)).click()
-
-    // });
 
     // Execute a block of code after every test
     afterEach(() => {
